fef5e68d2516249fde795b8184730586e006ed6e
Adam Brown <adam.brown.betty@gmail.com>
2019-08-01 23:13:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,6 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for] = ["Unix", "Assembly"]
+  programmer_hash[:dennis_ritchie][:known_for] = "Unix", "Assembly"
   return programmer_hash
 end
